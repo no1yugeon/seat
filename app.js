f93888ce78be18ca332f688e31af3c242cae2102
no1yugeon <jh42yg@gmail.com>
2023-05-28 20:38:34 +0900

    alpha 0.2

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-girlName = [
+const girlName = [
+    "none",
     "강나연",
     "김가을",
     "김성주",
@@ -14,9 +15,10 @@ girlName = [
     "정시현",
     "조은채",
     "한정인"
-]
+];
 
-boyName = [
+const boyName = [
+    "none",
     "강준영",
     "김강민",
     "김민준",
@@ -33,4 +35,45 @@ boyName = [
     "정민재",
     "조현호",
     "최준혁"
-]
+];
+
+changedGirl = [];
+changedBoy = [];
+check = [];
+all = [];
+
+for (let i = 1; i <= 15; i++) {
+    check[randomed = Math.random() * 15]++;
+    if (check[randomed] >= 1) {
+        i--;
+        continue;
+    }
+    else changedGirl[i] = girlName[randomed];
+}
+
+for (let i = 1; i <= 15; i++) {
+    check[i] = 0;
+}
+
+for (let i = 1; i <= 16; i++) {
+    check[randomed = Math.random() * 16]++;
+    if (check[randomed] >= 1) {
+        i--;
+        continue;
+    }
+    else changedBoy[i] = boyName[randomed];
+}
+
+for (let i = 1; i <= 31; i++) {
+    check[i] = 0;
+    if (i % 2 == 1) {
+        all[i] = changedGirl[i / 2];
+    }
+    else {
+        all[i] = changedBoy[i / 2 + 1];
+    }
+}
+
+for (let i = 1; i <= 31; i++) {
+    document.getElementsByClassName(toString(i)).innerText = all[i];
+}
